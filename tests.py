2153a120ccf3df32ad34b5a326b6222b98349cbc
Nikita Maslennikov <kulo6ut@yandex.ru>
2025-12-04 14:45:54 +0700

    new_commit_1

--- tests.py
+++ tests.py
@@ -1,27 +1,8 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
     def test_add_new_book_add_book(self):
         collector = BooksCollector()
@@ -101,13 +82,12 @@ class TestBooksCollector:
         collector.delete_book_from_favorites('Приключения Шерлока Холмса')
         assert 'Приключения Шерлока Холмса' not in collector.get_list_of_favorites_books()
 
-    @pytest.mark.parametrize('book_name, genre', [
-        ('Приключения Шерлока Холмса', 'Детективы'),
-        ('Маугли', 'Мультфильмы')
+    @pytest.mark.parametrize('book_name', [
+        'Приключения Шерлока Холмса',
+        'Маугли'
     ])
-    def test_get_list_of_favorites_books_have_favorites_books(self, book_name, genre):
+    def test_get_list_of_favorites_books_have_favorites_books(self, book_name):
         collector = BooksCollector()
         collector.add_new_book(book_name)
-        collector.set_book_genre(book_name, genre)
         collector.add_book_in_favorites(book_name)
         assert book_name in collector.get_list_of_favorites_books()
